e765d8890196f6ddb8a64bbc786982253eda41f4
Ludovic DEHON <tchiot.ludo@gmail.com>
2022-07-07 17:10:01 +0200

    Initial commit

---
+++ src/main/java/io/kestra/plugin/servicenow/AbstractServiceNow.java
@@ -0,0 +1,135 @@
+package io.kestra.plugin.servicenow;
+
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.Task;
+import io.kestra.core.runners.RunContext;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.*;
+import io.micronaut.http.client.DefaultHttpClientConfiguration;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Map;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+public abstract class AbstractServiceNow extends Task  {
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "ServiceNow domain.",
+        description = "Will be used to generate the url: `https://[[DOMAIN]].service-now.com/`"
+    )
+    @PluginProperty(dynamic = true)
+    private String domain;
+
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "ServiceNow username."
+    )
+    @PluginProperty(dynamic = true)
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "ServiceNow password."
+    )
+    @PluginProperty(dynamic = true)
+    private String password;
+
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "ServiceNow client ID."
+    )
+    @PluginProperty(dynamic = true)
+    private String clientId;
+
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "ServiceNow client secret."
+    )
+    @PluginProperty(dynamic = true)
+    private String clientSecret;
+
+    @Getter(AccessLevel.NONE)
+    private transient String token;
+
+    protected HttpClient client(String base) throws IllegalVariableEvaluationException, MalformedURLException, URISyntaxException {
+        DefaultHttpClientConfiguration configuration = new DefaultHttpClientConfiguration();
+        configuration.setConnectTimeout(Duration.ofSeconds(60));
+        configuration.setReadTimeout(Duration.ofSeconds(60));
+        configuration.setReadIdleTimeout(Duration.ofSeconds(60));
+
+        return HttpClient.create(URI.create(base).toURL(), configuration);
+    }
+
+    private String baseUri(RunContext runContext) throws IllegalVariableEvaluationException {
+        return "https://" + runContext.render(this.domain) + ".service-now.com/";
+    }
+
+    private String token(RunContext runContext) throws IllegalVariableEvaluationException, MalformedURLException, URISyntaxException {
+        if (this.token != null) {
+            return this.token;
+        }
+
+        MutableHttpRequest<String> request = HttpRequest.create(HttpMethod.POST, "/oauth_token.do")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body("grant_type=password" +
+                "&client_id=" + runContext.render(this.clientId) +
+                "&client_secret=" + runContext.render(this.clientSecret) +
+                "&username=" + runContext.render(this.username) +
+                "&password=" + URLEncoder.encode(runContext.render(this.password), StandardCharsets.UTF_8)
+            );
+
+        try (HttpClient client = this.client(this.baseUri(runContext))) {
+            HttpResponse<Map<String, String>> exchange = client.toBlocking().exchange(request, Argument.mapOf(String.class, String.class));
+
+            Map<String, String> token = exchange.body();
+
+            if (token == null || !token.containsKey("access_token")) {
+                throw new IllegalStateException("Invalid token request with response " + token);
+            }
+            this.token = token.get("access_token");
+
+            return this.token;
+        }
+    }
+
+    protected <REQ, RES> HttpResponse<RES> request(RunContext runContext, MutableHttpRequest<REQ> request, Argument<RES> argument) throws HttpClientResponseException {
+        try {
+            request = request
+                .bearerAuth(this.token(runContext))
+                .contentType(MediaType.APPLICATION_JSON);
+
+            try (HttpClient client = this.client(this.baseUri(runContext))) {
+                return client.toBlocking().exchange(request, argument);
+            }
+        } catch (HttpClientResponseException e) {
+            throw new HttpClientResponseException(
+                "Request failed '" + e.getStatus().getCode() + "' and body '" + e.getResponse().getBody(String.class).orElse("null") + "'",
+                e.getResponse()
+            );
+        } catch (IllegalVariableEvaluationException | MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/io/kestra/plugin/servicenow/Post.java
@@ -0,0 +1,110 @@
+package io.kestra.plugin.servicenow;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.runners.RunContext;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.uri.UriTemplate;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.slf4j.Logger;
+
+import java.util.Map;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Insert data inside a ServiceNow table."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Create an incident.",
+            code = {
+                "domain: \"[[YOUR_SNOW_DOMAIN]]\"",
+                "username: \"[[YOUR_SNOW_CLIENTID]]\"",
+                "password: \"[[YOUR_SNOW_CLIENTID]]\"",
+                "clientId: \"[[YOUR_SNOW_CLIENTID]]\"",
+                "clientSecret: \"[[YOUR_SNOW_CLIENTID]]\"",
+                "table: incident",
+                "data:",
+                "  short_description: API Create Incident...",
+                "  requester_id: f8266e2adb16fb00fa638a3a489619d2",
+                "  requester_for_id: a7ec77cbdefac300d322d182689619dc",
+                "  product_id: 01a2e3c1db15f340d329d18c689ed922\n",
+            }
+        )
+    }
+)
+public class Post extends AbstractServiceNow implements RunnableTask<Post.Output> {
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "ServiceNow table."
+    )
+    @PluginProperty(dynamic = true)
+    private String table;
+
+    @NotNull
+    @NotEmpty
+    @Schema(
+        title = "The data to insert."
+    )
+    @PluginProperty(dynamic = true)
+    private Map<String, Object> data;
+
+    @Override
+    public Post.Output run(RunContext runContext) throws Exception {
+        Logger logger = runContext.logger();
+
+        HttpResponse<PostResult> request = this.request(
+            runContext,
+            HttpRequest
+                .create(
+                    HttpMethod.POST,
+                    UriTemplate.of("/api/now/table/{table}")
+                        .expand(Map.of(
+                            "table", runContext.render(this.table)
+                        ))
+                )
+                .body(runContext.render(data)),
+            Argument.of(PostResult.class)
+        );
+
+        if (request.getBody().isEmpty()) {
+            throw new IllegalStateException("Empty body on '" + request + "'");
+        }
+
+        logger.info("Post done with result '{}'", request.getBody().orElse(null));
+
+        return Output.builder()
+            .result(request.getBody().get().getResult())
+            .build();
+    }
+
+    @Builder
+    @Getter
+    public static class Output implements io.kestra.core.models.tasks.Output {
+        @Schema(
+            title = "The result data.."
+        )
+        private Map<String, Object> result;
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class PostResult {
+        Map<String, Object> result;
+    }
+}
