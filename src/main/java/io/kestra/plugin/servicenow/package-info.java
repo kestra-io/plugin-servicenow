b1a1b90ba215d22412405455df4211fed13d7cd8
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:38:19 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/servicenow/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using ServiceNow.\n" +
+        "ServiceNow allows employees to work the way they want to, not how software dictates they have to.",
+    categories = { PluginSubGroup.PluginCategory.ALERTING, PluginSubGroup.PluginCategory.MISC } //FIXME TOOL
+)
+package io.kestra.plugin.servicenow;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
