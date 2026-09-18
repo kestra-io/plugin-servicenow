5db17ad4381688d45fa31297e3aa1cc78bcf12b6
brian.mulier <bmmulier@hotmail.fr>
2026-09-18 18:39:25 +0200

    feat(servicenow): advertise the record-creation task as a ticketing task

--- src/main/java/io/kestra/plugin/servicenow/Post.java
+++ src/main/java/io/kestra/plugin/servicenow/Post.java
@@ -11,6 +11,7 @@ import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.models.tasks.TicketingTaskInterface;
 import io.kestra.core.runners.RunContext;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -76,7 +77,7 @@ import io.kestra.core.models.annotations.PluginProperty;
         )
     }
 )
-public class Post extends AbstractServiceNow implements RunnableTask<Post.Output> {
+public class Post extends AbstractServiceNow implements RunnableTask<Post.Output>, TicketingTaskInterface {
     @NotNull
     @Schema(
         title = "ServiceNow table",
